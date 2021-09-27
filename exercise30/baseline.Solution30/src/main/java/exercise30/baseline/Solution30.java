edc30589be9f1ac2d7d3609085f45321a8cc9f20
Keven Laptop <k.fazio@knights.ucf.edu>
2021-09-27 11:15:56 -0400

    ex30 Initial commit

---
+++ exercise30/baseline.Solution30/src/main/java/exercise30/baseline/Solution30.java
@@ -0,0 +1,31 @@
+package exercise30.baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Keven Fazio
+ */
+
+public class Solution30 {
+
+    /*
+     * Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
+     *
+     * Constraint
+     * -Use a nested loop to complete this program.
+     * -Align each column within the table without using the tab character.
+     */
+
+    public static void main(String[] args) {
+        Solution30 table = new Solution30();
+
+        //create multiplicationTable
+        table.multiplicationTable();
+    }
+
+    public void multiplicationTable(){
+        //create a 12x12 multiplication table using int row and int column
+        //for row = 1, row <= 12, ++row
+            //for column = 1, column <= 12, ++column
+                //print table
+    }
+}
