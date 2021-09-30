92acd4cb07ca4ad3c41fbea159eaf56921e72ee9
Keven Laptop <k.fazio@knights.ucf.edu>
2021-09-30 18:30:17 -0400

    Initial commit

--- exercise33/baseline.Solution33/src/main/java/exercise33/baseline/Solution33.java
+++ exercise33/baseline.Solution33/src/main/java/exercise33/baseline/Solution33.java
@@ -5,7 +5,13 @@ package exercise33.baseline;
  *  Copyright 2021 Keven Fazio
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution33 {
+    Scanner input = new Scanner(System.in);
 
     /*
      * Create a Magic 8 Ball game that prompts for a question and
@@ -23,6 +29,36 @@ public class Solution33 {
      */
 
     public static void main(String[] args) {
+        Solution33 app = new Solution33();
+
+        //prompt user "What's your question?"
+        String question = app.scanInput("What's your question? ");
+
+        //get pseudo random number
+        int randomNumber = app.getRandomNumber();
+
+        //print getResponse
+        System.out.print(app.getResponse());
 
     }
+
+    public String scanInput (String prompt){
+        //Scan input and return
+        System.out.print(prompt);
+        return input.nextLine();
+    }
+
+    public int getRandomNumber (){
+        //get random number with .nextInt(4) and return
+        Random num = new Random();
+        return num.nextInt(4);
+    }
+
+    /*
+    public String getResponse (){
+        //
+        ArrayList<String> positiveResponses = new ArrayList<String> (Arrays.asList("Yes",
+                "No", "Maybe", "Ask again Later"));
+    }
+     */
 }
